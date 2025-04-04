aa4ccb45496b12f161d0815b7b7eef84e009d046
Esma-LouisBl <l.blondeau@rns.ecolescreatives.com>
2025-04-04 15:49:15 +0200

    First dialogue function iteration

---
+++ Assets/Scripts/Dialogue.cs
@@ -0,0 +1,64 @@
+using UnityEngine;
+using TMPro;
+using System.Collections;
+
+public class Dialogue : MonoBehaviour
+{
+    public TextMeshProUGUI textComponent;
+    public string[] lines;
+    public float textSpeed;
+
+    private int _index;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        textComponent.text = string.Empty;
+        StartDialogue();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (textComponent.text == lines[_index])
+            {
+                NextLine();
+            }
+            else
+            {
+                StopAllCoroutines();
+                textComponent.text = lines[_index];
+            }
+        }
+    }
+
+    public void StartDialogue()
+    {
+        _index = 0;
+        StartCoroutine(TypeLine());
+    }
+
+    public IEnumerator TypeLine()
+    {
+        foreach (char c in lines[_index].ToCharArray())
+        {
+            textComponent.text += c;
+            yield return new WaitForSeconds(textSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        if (_index < lines.Length - 1)
+        {
+            _index++;
+            textComponent.text = string.Empty;
+            StartCoroutine(TypeLine());
+        }
+        else
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
