b43bd15c7f942603805b782da586a7c05cb7e63f
Esma-LouisBl <l.blondeau@rns.ecolescreatives.com>
2025-05-06 17:18:19 +0200

    avancée carnet de preuves

---
+++ Assets/Scripts/Controllers/EvidencesController.cs
@@ -0,0 +1,55 @@
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+
+public class EvidencesController : MonoBehaviour
+{
+    private int _evidenceIndex;
+
+    [SerializeField]
+    private GameObject _window;
+    [SerializeField]
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (_window.activeSelf)
+            {
+                _window.SetActive(false);
+            }
+            else
+            {
+                _window.SetActive(true);
+            }
+        }
+    }
+
+    public void ChangeEvidenceUp()
+    {
+        if (_evidenceIndex < 2)
+        {
+            _evidenceIndex++;
+        }
+        else
+        {
+            _evidenceIndex = 0;
+        }
+    }
+
+    public void ChangeEvidenceDown()
+    {
+        if (_evidenceIndex > 0)
+        {
+            _evidenceIndex--;
+        }
+        else
+        {
+            _evidenceIndex = 2;
+        }
+    }
+}
