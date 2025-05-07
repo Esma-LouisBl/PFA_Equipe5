7b33e58656269dd0934d0e3244afe91d0626e8f3
IAmSomeoneWhoAmI <thomas.bouchard.dubois@gmail.com>
2025-05-07 13:09:21 +0200

    Tableau

---
+++ Assets/Scripts/InteractiveBoard.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+
+public class InteractiveBoard : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 offset;
+    private float x;
+
+    
+    public Collider waypointArea;
+
+    void Start()
+    {
+        x = transform.position.x;
+    }
+
+    void Update()
+    {
+        Vector3 pos = transform.position;
+        pos.x = x;
+        transform.position = pos;
+    }
+
+    void OnMouseDown()
+    {
+        offset = transform.position - MouseWorldPosition();
+    }
+
+    void OnMouseDrag()
+    {
+        
+        Vector3 newPosition = MouseWorldPosition() + offset;
+
+        
+        newPosition = ClampToWaypointArea(newPosition);
+
+        
+        transform.position = newPosition;
+    }
+
+    Vector3 MouseWorldPosition()
+    {
+        var mouseScreenPos = Input.mousePosition;
+        mouseScreenPos.z = Camera.main.WorldToScreenPoint(transform.position).z;
+        return Camera.main.ScreenToWorldPoint(mouseScreenPos);
+    }
+
+    
+    Vector3 ClampToWaypointArea(Vector3 position)
+    {
+        
+        Bounds bounds = waypointArea.bounds;
+
+        
+        position.x = Mathf.Clamp(position.x, bounds.min.x, bounds.max.x);
+        position.y = Mathf.Clamp(position.y, bounds.min.y, bounds.max.y);
+        position.z = Mathf.Clamp(position.z, bounds.min.z, bounds.max.z);
+
+        return position;
+    }
+}
+
