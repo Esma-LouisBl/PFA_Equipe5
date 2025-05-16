8cdc8b1331dfebc3140cf422609fc28ed16e6607
Esma-LouisBl <l.blondeau@rns.ecolescreatives.com>
2025-05-16 11:34:48 +0200

    intégration nouveau LD

--- Assets/Scripts/Test/DeskInteraction.cs
+++ Assets/Scripts/Test/DeskInteraction.cs
@@ -54,7 +54,6 @@ public class DeskInteraction : MonoBehaviour
                 {
                     LoadScene(_evidencesSystem.currentEvidence.ReactionInspector);
                 }
-                Destroy(_currentEvidence);
             }
         }
     }
