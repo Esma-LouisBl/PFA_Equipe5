d0790e5a0f40410296ab739745dbf8e79a4c9a67
Esma-LouisBl <l.blondeau@rns.ecolescreatives.com>
2025-05-20 11:00:40 +0200

    déplacement vers le bureau des preuves

--- Assets/Scripts/NewMovement.cs
+++ Assets/Scripts/NewMovement.cs
@@ -12,11 +12,17 @@ public class NewMovement : MonoBehaviour
     private GameManager _gameManager;
     [SerializeField]
     private MeshOutliner _outliner;
+    [SerializeField]
+    private CursorController _cursorController;
 
     [SerializeField]
-    private float _speed;
-
+    private float _speed = 20f;
+    [SerializeField]
+    private float _rotationSpeed = 350f;
     private Quaternion _targetRotation;
+
+    [SerializeField]
+    private GameObject _realCabinet, _falseCabinet;
 
     private void Update()
     {
@@ -32,10 +38,14 @@ public class NewMovement : MonoBehaviour
     private void MoveToCabinet()
     {
         StartCoroutine(Moving());
+        _realCabinet.SetActive(false);
+        _falseCabinet.SetActive(true);
+        _outliner.selectedCabinet = false;
     }
 
     private IEnumerator Moving()
     {
+        _cursorController.EnableCursor(false);
         while (Vector3.Distance(_playerTransform.position, _waypoint.position) > 0.01f)
         {
             _playerTransform.position = Vector3.MoveTowards(_playerTransform.position, _waypoint.position, _speed * Time.deltaTime);
@@ -47,6 +57,20 @@ public class NewMovement : MonoBehaviour
             }
             yield return new WaitForSeconds(0.01f);
         }
+
         _targetRotation = Quaternion.Euler(0f, _playerTransform.eulerAngles.y + 90f, 0f);
+
+        while (Quaternion.Angle(_playerTransform.rotation, _targetRotation) > 1f)
+        {
+            _playerTransform.rotation = Quaternion.RotateTowards(_playerTransform.rotation, _targetRotation, _rotationSpeed * Time.deltaTime);
+            
+            if (Quaternion.Angle(_playerTransform.rotation, _targetRotation) < 1f)
+            {
+                _playerTransform.rotation = _targetRotation;
+
+                _cursorController.EnableCursor(true);
+            }
+            yield return new WaitForSeconds(0.005f);
+        }
     }
 }
